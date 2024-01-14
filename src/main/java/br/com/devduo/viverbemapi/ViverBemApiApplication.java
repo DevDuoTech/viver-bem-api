002fedc1e9b0a5623aa09cc8b5ce64180feeb7b8
matheusslr <matheusslr@hotmail.com>
2024-01-14 13:23:26 -0300

    First commit

---
+++ src/main/java/br/com/devduo/viverbemapi/ViverBemApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.devduo.viverbemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViverBemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViverBemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devduo/viverbemapi/ViverBemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devduo.viverbemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViverBemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
